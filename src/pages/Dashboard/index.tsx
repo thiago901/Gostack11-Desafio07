dffddf66aab481d71d33388b61d0038c32a47440
Thiago Santos <tsrocha901@gmail.com>
2020-07-10 16:53:12 -0300

    first commit

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,122 @@
+import React, { useState, useEffect } from 'react';
+
+import income from '../../assets/income.svg';
+import outcome from '../../assets/outcome.svg';
+import total from '../../assets/total.svg';
+
+import api from '../../services/api';
+
+import Header from '../../components/Header';
+
+import formatValue from '../../utils/formatValue';
+
+import { Container, CardContainer, Card, TableContainer } from './styles';
+
+interface Transaction {
+  id: string;
+  title: string;
+  value: number;
+  formattedValue: string;
+  formattedDate: string;
+  type: 'income' | 'outcome';
+  category: { title: string };
+  created_at: Date;
+}
+
+interface Balance {
+  income: string;
+  outcome: string;
+  total: string;
+}
+
+const Dashboard: React.FC = () => {
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+  const [balance, setBalance] = useState<Balance>({} as Balance);
+
+  useEffect(() => {
+    async function loadTransactions(): Promise<void> {
+      // TODO
+      const response = await api.get('/transactions');
+      const { data } = response;
+
+      const transaction: Transaction[] = data.transactions;
+
+      setTransactions(
+        transaction.map(a => ({
+          ...a,
+          formattedDate: new Date(a.created_at).toLocaleDateString('pt-br'),
+          formattedValue: formatValue(a.value),
+        })),
+      );
+
+      const dataBalance: Balance = data.balance;
+      setBalance({
+        income: formatValue(Number(dataBalance.income)),
+        outcome: formatValue(Number(dataBalance.outcome)),
+        total: formatValue(Number(dataBalance.total)),
+      });
+    }
+
+    loadTransactions();
+  }, []);
+
+  return (
+    <>
+      <Header />
+      <Container>
+        <CardContainer>
+          <Card>
+            <header>
+              <p>Entradas</p>
+              <img src={income} alt="Income" />
+            </header>
+            <h1 data-testid="balance-income">{balance.income}</h1>
+          </Card>
+          <Card>
+            <header>
+              <p>Saídas</p>
+              <img src={outcome} alt="Outcome" />
+            </header>
+            <h1 data-testid="balance-outcome">{balance.outcome}</h1>
+          </Card>
+          <Card total>
+            <header>
+              <p>Total</p>
+              <img src={total} alt="Total" />
+            </header>
+            <h1 data-testid="balance-total">{balance.total}</h1>
+          </Card>
+        </CardContainer>
+
+        <TableContainer>
+          <table>
+            <thead>
+              <tr>
+                <th>Título</th>
+                <th>Preço</th>
+                <th>Categoria</th>
+                <th>Data</th>
+              </tr>
+            </thead>
+
+            <tbody>
+              {transactions.map(t => (
+                <tr key={t.id}>
+                  <td className="title">{t.title}</td>
+                  <td className={t.type}>
+                    {t.type === 'outcome' && ' - '}
+                    {t.formattedValue}
+                  </td>
+                  <td>{t.category.title}</td>
+                  <td>{t.formattedDate}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </TableContainer>
+      </Container>
+    </>
+  );
+};
+
+export default Dashboard;
